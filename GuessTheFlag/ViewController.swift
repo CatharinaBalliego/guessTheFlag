66e273363c8f22c0c05fa9870415e798d7547b6a
Catharina Balliego <catharina_@hotmail.com.br>
2023-03-27 14:45:29 -0300

    shuffling countries array and show title

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet var btn_option1: UIButton!
     @IBOutlet var btn_option2: UIButton!
     @IBOutlet var btn_option3: UIButton!
-    
+        
     var score = 0
     var countries = [String]()
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,16 +32,21 @@ class ViewController: UIViewController {
         
         //adjust image border to remove margin
         btn_option1.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-        btn_option1.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-        btn_option1.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        btn_option2.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        btn_option3.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
         
         askQuestion()
     }
 
     func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         btn_option1.setImage(UIImage(named: countries[0]), for: .normal)
         btn_option2.setImage(UIImage(named: countries[1]), for: .normal)
         btn_option3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
     
 }
