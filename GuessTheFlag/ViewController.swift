4aff87fe06c9ff1433430d50022fcd8b89e33cbb
Catharina Balliego <catharina_@hotmail.com.br>
2023-03-27 13:42:02 -0300

    initial setup

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btn_option1: UIButton!
+    @IBOutlet var btn_option2: UIButton!
+    @IBOutlet var btn_option3: UIButton!
+    
+    var score = 0
+    var countries = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia","france","nigeria","uk","us","russia", "italy","spain", "germany","ireland","monaco","poland"]
+        
+        btn_option1.layer.borderWidth = 1
+        btn_option2.layer.borderWidth = 1
+        btn_option3.layer.borderWidth = 1
+        
+        btn_option1.layer.borderColor = UIColor.lightGray.cgColor
+        btn_option2.layer.borderColor = UIColor.lightGray.cgColor
+        btn_option3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
-
+    func askQuestion(){
+        btn_option1.setImage(UIImage(named: countries[0]), for: .normal)
+        btn_option2.setImage(UIImage(named: countries[1]), for: .normal)
+        btn_option3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
 }
 
+	
